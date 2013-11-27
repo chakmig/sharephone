5540f30935fd24d95197413ad95f591bf28efabd
chakmig <chakmig@hotmail.com>
2013-11-27 13:26:17 +0100

    basic structure

---
+++ app.js
@@ -0,0 +1,15 @@
+// Move to main
+document.querySelector('#go-dialer').addEventListener('click', function (ev) {
+  //console.log(ev.target)
+  document.querySelector('#dialer').classList.add('move-to-main');
+});
+document.querySelector('#dialer .back').addEventListener('click', function (ev) {
+  console.log(ev.target)
+  document.querySelector('#dialer').classList.remove('move-to-main');
+});
+
+
+
+//document.querySelector('#action-menu').addEventListener ('click', function () 
+//  document.querySelector('#action-menu').classList.add('move-to-right');
+//});
