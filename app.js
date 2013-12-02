3d0e8f66e5286846818a25ec23cd3cffe586cea6
rnowm <arnau@arnaumarch.com>
2013-12-02 15:49:54 +0100

    added logic for "close" button

--- app.js
+++ app.js
@@ -14,12 +14,17 @@ for (var i = 0; i < homeNav.length; i++) {
 var sectionNav = document.querySelectorAll("[data-nav]");
 for (var i = 0; i < sectionNav.length; i++) {
   sectionNav[i].addEventListener("click", function(ev) {
-    if (this.dataset.nav === "home") {
+    switch(this.dataset.nav) {
+    case "home":
       this.parentNode.classList.remove('move-to-main');
       document.querySelector("#home").classList.remove('move-to-left');
-    } else {
+      break;
+    case "close":
+      this.parentNode.classList.remove('move-to-main');
+      break;
+    default:
       document.querySelector('#home').classList.add('move-to-left');
-      document.querySelector('#' + this.dataset.nav).classList.add('move-to-main');     
+      document.querySelector('#' + this.dataset.nav).classList.add('move-to-main');
     }
   });
 }
